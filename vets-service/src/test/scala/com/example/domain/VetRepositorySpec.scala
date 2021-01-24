471c51971bf65999437a176bf62cbbf16d6a5bbc
Kaur Maetas <kaur.matas@flipp.com>
2021-01-24 03:53:52 -0800

    minor refactorings

--- vets-service/src/test/scala/com/example/domain/VetRepositorySpec.scala
+++ vets-service/src/test/scala/com/example/domain/VetRepositorySpec.scala
@@ -1,7 +1,6 @@
 package com.example.domain
 
-import scala.util.Random
-
+import com.example.domain.Specialty
 import com.example.domain.Vet
 import com.example.domain.VetRepository
 import com.example.model.VetDao
@@ -18,26 +17,31 @@ object VetRepositorySpec extends DefaultRunnableSpec {
   def spec = suite("VetRepository")(
     testM("should return list of Vets") {
 
-      checkM(Gen.mapOf(Gen.anyInt, DeriveGen[Vet])) {
-        someGenVets =>
-          val vets = someGenVets.map { case (k, v) => k -> v.copy(id = k) }.values.toList
+      val vetsWithSpecialtiesGen =
+        Gen
+          .listOf((DeriveGen[MVet] <&> Gen.listOf(DeriveGen[MSpecialty])))
+          .map(_.distinctBy(_._1.id))
 
+      checkM(vetsWithSpecialtiesGen) {
+        vetsWithSpecialties =>
           assertM(VetRepository.all)(
-            hasSameElements(vets)
+            hasSameElements(vetsWithSpecialties.map {
+              case (mVet, mSpecialties) =>
+                Vet(
+                  mVet.id,
+                  mVet.firstName,
+                  mVet.lastName,
+                  mSpecialties.map(ms => Specialty(ms.name))
+                )
+            })
           ).provideLayer(
             ZLayer.fromEffect(
               Ref.make {
-                vets.flatMap { vet =>
-                  if (vet.specalities.isEmpty)
-                    List(MVet(vet.id, vet.fistName, vet.lastName) -> None)
-                  else
-                    vet.specalities.foldRight(List.empty[(MVet, Option[MSpecialty])]) {
-                      case (s, acc) =>
-                        (MVet(vet.id, vet.fistName, vet.lastName) -> Some(
-                          MSpecialty(Random.nextInt(), s.name)
-                        )) +: acc
-                    }
-
+                vetsWithSpecialties.flatMap {
+                  case (mvet, Nil) =>
+                    List((mvet -> None))
+                  case (mvet, mSpecialties) =>
+                    mSpecialties.map(s => mvet -> Some(s))
                 }
               }
             ) >>> VetDao.inMemory >>> VetRepository.live
